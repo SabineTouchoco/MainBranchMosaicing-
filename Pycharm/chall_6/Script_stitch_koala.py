3c8d125bf1a7fbdc24dc4b5053eacb60ff4e7a8c
Thibaut <thibaut.colome@insa-lyon.fr>
2018-10-02 18:39:52 +0200

    gitignore update + deleted useless code from Koala script

--- Pycharm/chall_6/Script_stitch_koala.py
+++ Pycharm/chall_6/Script_stitch_koala.py
@@ -1,8 +1,4 @@
 import cv2
-import os
-#dir_ = os.path.dirname(__file__)
-#filename0 = os.path.join(dir_, "ressources/koala_0.png")
-#filename1 = os.path.join(dir_, "ressources/koala_1.png")
 
 stitcher = cv2.createStitcher(False)
 img0 = cv2.imread("ressources/koala_0.png")
