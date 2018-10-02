e4fce4982bb6b1bdd34d6c7b82e8cb4683549dbb
SabineIsaac <sabine-esi@hotmail.fr>
2018-10-02 20:47:20 +0200

    Nouvelle architecture.

--- Pycharm/chall_6/Script_stitch_koala.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-stitcher = cv2.createStitcher(False)
-img0 = cv2.imread("ressources/koala_0.png")
-img1 = cv2.imread("ressources/koala_1.png")
-result = stitcher.stitch((img0, img1))
-
-cv2.imwrite("ressources/koala_result.png", result[1])
