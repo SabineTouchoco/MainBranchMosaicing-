936f169bdc12e612e8a6702ba3fea5d29cfff6e6
SabineIsaac <sabine-esi@hotmail.fr>
2018-10-14 17:45:03 +0200

    RANSAC alignement tested : trapezoidal image obtained.

--- Python_FrameFromVideo/Mosa.py
+++ Python_FrameFromVideo/Mosa.py
@@ -1,18 +1,84 @@
+from __future__ import print_function
 import cv2
-import os
 import numpy as np
 
-stitcher = cv2.createStitcher(False)
-img0 = cv2.imread("Outputs//frame0.jpg")
-img1 = cv2.imread("Outputs//frame1.jpg")
-img2 = cv2.imread("Outputs//frame2.jpg")
-img3 = cv2.imread("Outputs//frame3.jpg")
-img4 = cv2.imread("Outputs//frame4.jpg")
-img5 = cv2.imread("Outputs//frame5.jpg")
-img6 = cv2.imread("Outputs//frame6.jpg")
-img7 = cv2.imread("Outputs//frame7.jpg")
-img8 = cv2.imread("Outputs//frame8.jpg")
-img9 = cv2.imread("Outputs//frame9.jpg")
-img10 = cv2.imread("Outputs//frame10.jpg")
-result = stitcher.stitch((img1, img2, img3, img4, img5))
-cv2.imwrite("Outputs//Mosaique.jpg", result[1])
+MAX_FEATURES = 500
+GOOD_MATCH_PERCENT = 0.15
+
+
+def align_images(im1, im2):
+    # Convert images to gray scale
+    im1gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+    im2gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+
+    # Detect ORB features and compute descriptors.
+    orb = cv2.ORB_create(MAX_FEATURES)
+    kp1, descriptors1 = orb.detectAndCompute(im1gray, None)
+    kp2, descriptors2 = orb.detectAndCompute(im2gray, None)
+
+    # Match features.
+    matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+    matches = matcher.match(descriptors1, descriptors2, None)
+
+    # Sort matches by score
+    matches.sort(key=lambda x: x.distance, reverse=False)
+
+    # Remove not so good matches
+    numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
+    matches = matches[:numGoodMatches]
+
+    # Draw top matches
+    imMatches = cv2.drawMatches(im1, kp1, im2, kp2, matches, None)
+    cv2.imwrite("Outputs/ResultMatches.jpg", imMatches)
+
+    # Extract location of good matches
+    points1 = np.zeros((len(matches), 2), dtype=np.float32)
+    points2 = np.zeros((len(matches), 2), dtype=np.float32)
+
+    for i, match in enumerate(matches):
+        points1[i, :] = kp1[match.queryIdx].pt
+        points2[i, :] = kp2[match.trainIdx].pt
+
+    # Find homography
+    h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
+
+    # Use homography
+    height, width, channels = im2.shape
+    im1Reg = cv2.warpPerspective(im1, h, (width, height))
+
+    return im1Reg, h
+
+
+if __name__ == '__main__':
+    # Read reference image
+    refFilename = "Outputs/frame0.jpg"
+    print("Reading reference image : ", refFilename)
+    imReference = cv2.imread(refFilename, cv2.IMREAD_COLOR)
+
+    # Read image to be aligned
+    imFilename = "Outputs/frame1.jpg"
+    print("Reading image to align : ", imFilename);
+    im = cv2.imread(imFilename, cv2.IMREAD_COLOR)
+
+    print("Aligning images ...")
+    # Registered image will be resotred in imReg.
+    # The estimated homography will be stored in h.
+    imReg, h = align_images(im, imReference)
+
+    # Write aligned image to disk.
+    outFilename = "Outputs/ResultAligned.jpg"
+    print("Saving aligned image : ", outFilename);
+    cv2.imwrite(outFilename, imReg)
+
+    # Print estimated homography
+    print("Estimated homography : \n", h)
+
+#ORB
+
+#FLANN
+
+#RANSAC
+
+
+#cv2.imwrite("Outputs//res0.jpg", res0)
+#cv2.imwrite("Outputs//res1.jpg", res1)
