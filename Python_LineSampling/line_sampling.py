44546b69bd858c17f81cd615efc02ab7d94c529a
Unknown <thibaut.colome@insa-lyon.fr>
2018-11-05 17:25:40 +0100

    Updated line sampling

--- Python_LineSampling/line_sampling.py
+++ Python_LineSampling/line_sampling.py
@@ -5,33 +5,35 @@ filename = "inputs/T4.avi"
 
 video = cv2.VideoCapture(filename)
 fps = video.get(cv2.CAP_PROP_FPS)
-print('Video fps : ', fps)
+print('Video fps : ' + str(fps))
 
 width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
-print('Video width : ', width)
+print('Video width : ' + str(width))
 
 frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
-print('Video frame count : ', frame_count)
+print('Video frame count : ' + str(frame_count))
 
 count = 0
-delta = 20
 center = int(width/2)
 offset = 0
 for i in range(int(frame_count)):
     success, img = video.read()
-    if success:
+    if success and i > 120:
+        # delta = int(50/((count/100)+1))
+        delta = 80 - i * 0.12
+        delta = int(delta)
         line = img[:, (center - delta + offset):(center + delta + offset), :]
         if count > 0:
-            result = np.concatenate((result, line), 1)
+            result = np.concatenate((line, result), 1)
         else:
             result = line
-        #img_path = "outputs/img_" + str(count) + ".png"
-        #cv2.imwrite(img_path, img)
+        # img_path = "outputs/img_" + str(count) + ".png"
+        # cv2.imwrite(img_path, img)
         count += 1
         print("img " + str(i) + " sampled.")
 print("Done.")
 
-img_path = "outputs/result.png"
+img_path = "outputs/result_delta_var.png"
 cv2.imwrite(img_path, result)
 
 print("Image saved.")
